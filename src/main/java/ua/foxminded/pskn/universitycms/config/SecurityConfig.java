89a6f25fa99a0a356c7edf50c9e1eb2f5af909a5
vdmpskn <vadim.piskun.dp@gmail.com>
2023-08-01 21:19:49 +0300

    add AuthenticationSuccessHandler and a bit of style for pages

--- src/main/java/ua/foxminded/pskn/universitycms/config/SecurityConfig.java
+++ src/main/java/ua/foxminded/pskn/universitycms/config/SecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import ua.foxminded.pskn.universitycms.security.MyAuthenticationSuccessHandler;
 import ua.foxminded.pskn.universitycms.security.MyUserDetailsService;
 
 @Configuration
@@ -31,6 +33,7 @@ public class SecurityConfig {
             )
             .formLogin((form) -> form
                 .defaultSuccessUrl("/cabinet")
+                .successHandler(myAuthenticationSuccessHandler())
                 .loginPage("/login")
                 .permitAll()
             )
@@ -47,6 +50,11 @@ public class SecurityConfig {
         daoAuthenticationProvider.setPasswordEncoder(cryptPasswordEncoder());
 
         return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler myAuthenticationSuccessHandler(){
+        return new MyAuthenticationSuccessHandler();
     }
 
     @Bean
---
+++ src/main/java/ua/foxminded/pskn/universitycms/security/MyAuthenticationSuccessHandler.java
@@ -0,0 +1,33 @@
+package ua.foxminded.pskn.universitycms.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+
+public class MyAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
+        AuthenticationSuccessHandler.super.onAuthenticationSuccess(request, response, chain, authentication);
+    }
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        String role = authentication.getAuthorities().iterator().next().getAuthority();
+
+        if (role.equals("admin")) {
+            response.sendRedirect("/adminscab");
+        }
+        if (role.equals("professor")) {
+            response.sendRedirect("/professorscab");
+        }
+        if (role.equals("student")) {
+            response.sendRedirect("/studentscab");
+        }
+    }
+}
+
